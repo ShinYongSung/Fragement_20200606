22b74cfee7d74229e48a7763bcd5e904b0e718fb
ShinYongSUng <newxm@naver.com>
2020-06-06 12:26:46 +0900

    SecondFragment 추가 => 스크롤뷰 대응

---
+++ app/src/main/java/my/Shin/fragement_20200606/fragments/ThridFragment.kt
@@ -0,0 +1,23 @@
+package my.Shin.fragement_20200606.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import my.Shin.fragement_20200606.R
+import java.util.zip.Inflater
+
+class ThridFragment: Fragment () {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.)
+    }
+
+
+    }
+}
